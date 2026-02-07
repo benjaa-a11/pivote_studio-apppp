f0d1c4704b3e2e8d11b7ef7aa9d33a1861311776
benjaa-a11 <benjabostero223@gmail.com>
2026-02-07 02:40:58 -0300

    feat: Add initial Android application build configuration with Flutter, Firebase, and Media3 ExoPlayer dependencies.

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -25,7 +25,7 @@ android {
         applicationId = "com.example.pivote_studio"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
-        minSdk = 21
+        minSdk = 24
         targetSdk = 36
         versionCode = flutter.versionCode
         versionName = flutter.versionName
