7d7517604916034bd374d6243b16e9aeffc980e1
benjaa-a11 <benjabostero223@gmail.com>
2026-07-11 01:11:39 -0300

    deleted

--- custom_video_controls.dart
+++
@@ -1,8 +0,0 @@
-// Data Models - Video Player Optimization and Security
-export 'buffer_state.dart';
-export 'player_state.dart';
-export 'retry_attempt.dart';
-export 'security_event.dart';
-export 'stream_url.dart';
-export 'telemetry_event.dart';
-export 'video_quality.dart';
--- iptv_engine.dart
+++
@@ -1,5 +0,0 @@
-// Domain Layer - Video Player Optimization and Security
-export 'cdn_pattern_detector.dart';
-export 'optimized_url_resolver.dart';
-export 'resolved_url.dart';
-export 'url_cache.dart';
